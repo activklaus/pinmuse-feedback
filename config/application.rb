51fcf9badf28bf45571a636465595c39773d80f9
activklaus <activklaus@web.de>
2026-06-30 09:33:23 +0200

    added a frontend

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module PinmuseFeedback
   class Application < Rails::Application
     config.load_defaults 7.1
     config.api_only = true
+    config.public_file_server.enabled = true
 
     config.time_zone = 'Berlin'
     config.active_record.default_timezone = :utc
